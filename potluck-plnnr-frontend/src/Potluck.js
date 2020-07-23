7fb5f048a6481dfb54abba03cf0f6333e1242900
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-07-22 21:21:10 -0400

    fixed forms based on group mtg. Added functionality to potluck.

--- potluck-plnnr-frontend/src/Potluck.js
+++ potluck-plnnr-frontend/src/Potluck.js
@@ -1,25 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import * as yup from 'yup';
 
 const Potluck = () => {
+ 
+    const [host, setHost] = useState()
+    const [formState, setFormState] = useState({
+        name: '',
+        date: '',
+        time: '',
+        dish: false
+    })
+
+
+    let formSchema = yup.object().shape({
+        name: yup.string().required('Please provide your name'),
+        date: yup.string().required('Must choose a date.'),
+        time: yup.string().required('Must choose a time.'),
+        dish: yup.boolean().oneOf([true], 'Must check at least one box!')
+
+});
+
+
+
+    const formSubmit = e => {
+        e.preventDefault();
+        console.log('form submitted',);
+        axios.post('https://backend-bw.herokuapp.com/')
+            .then(response => {
+                setHost(response.data)
+                console.log(response.data)
+                console.log('form submitted success', host)
+             })
+        
+            .catch(err => console.log('error', err));
+    };
+
+    const validateChange = e => {
+        e.persist();
+
+    }
+
+    const inputChange = e => {
+        const value =
+            e.target.type === "checkbox" ? e.target.checked : e.target.value;
+        setFormState({ ...formState, [e.target.name]: value });
+        validateChange(e);
+    }
 
     return (
-        <div>Attendee Form
-            <form>
+        <div>Host Form
+            <form onSubmit={formSubmit}> 
                 <label htmlFor='name'>Name</label>
-                <input type='text' value='name' />
+                <input type='text' name='name' onChange={inputChange} value={formState.name}/>
                 <label htmlFor='date'>Date</label>
-                <input type='date' value='date' />
+                <input type='date'  min='2018-01-01' max='2030-12-31'/>
                 <label htmlFor='time'>Time</label>
-                <input type='time' value='time'/>
-                <label htmlFor='items'>Dish Provided</label>
-                <select name='items'>
-                    <option value='appetizer'>Appetizer</option>
-                    <option value='entree'>Entree</option>
-                    <option value='side'>Sides</option>
-                    <option value='dessert'>Dessert</option>
-                </select>
-                <label htmlFor='comments'>Comments/Remarks</label>
-                <textarea type='type' value='text'/>
+                <input type='time'/>
+                <label htmlFor='items'>Dishes Reqested</label>
+                <div>
+                    <input name='dish' type='checkbox'  />Appetizer
+                </div>
+                <div>
+                    <input name='dish' type='checkbox' />Entree
+                </div>
+                <div>
+                    <input name='dish' type='checkbox' />Sides
+                </div>
+                <div>
+                    <input name='dish' type='checkbox' /> Dessert
+                </div>
+                
+               
                 <button>Enter</button>
             </form>
         </div>
--- potluck-plnnr-frontend/src/Users.js
+++ potluck-plnnr-frontend/src/Users.js
@@ -9,12 +9,7 @@ const User = () => {
                     <input type='text' value='name'/>
                 <label htmlFor='password'>Password</label>
                     <input type='text' value='password'/>
-                <label htmlFor='role'>Role</label>
-                <select namee='role'>
-                    <option value='organizer'>Organizer</option>
-                    <option value='guest'>Guest</option>
-                    <option value='other'>Other</option>
-                </select>
+                               
                 <button>Enter</button>
             </form>
         </div>
